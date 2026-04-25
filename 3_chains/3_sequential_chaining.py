49a865d0e8b6d4be988a2f26f25f4aef5c55613f
Yash Chitale <114438423+yashchitale96@users.noreply.github.com>
2026-04-25 14:08:33 +0530

    sequential chaining

---
+++ 3_chains/3_sequential_chaining.py
@@ -0,0 +1,41 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableLambda
+from langchain_openai import ChatOpenAI
+
+# Load environment variables from .env
+load_dotenv()
+
+# Create a ChatOpenAI model
+model = ChatOpenAI(model="gpt-4o-mini")
+
+# Define prompt templates
+animal_facts_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You like telling facts and you tell facts about {animal}."),
+        ("human", "Tell me {count} facts."),
+    ]
+)
+
+# Define a prompt template for translation to French
+translation_template = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a translator and convert the provided text into {language}."),
+        ("human", "Translate the following text to {language}: {text}"),
+    ]
+)
+
+# Define additional processing steps using RunnableLambda
+count_words = RunnableLambda(lambda x: f"Word count: {len(x.split())}\n{x}")
+prepare_for_translation = RunnableLambda(lambda output: {"text": output, "language": "hindi"})
+
+
+# Create the combined chain using LangChain Expression Language (LCEL)
+chain = animal_facts_template | model | StrOutputParser() | prepare_for_translation | translation_template | model | StrOutputParser() 
+
+# Run the chain
+result = chain.invoke({"animal": "cat", "count": 2})
+
+# Output
+print(result)
